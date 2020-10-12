82b4696a355ba643730592a2ba6af031a2427f8d
Lee Burgess <lee.j.burges77@gmail.com>
2020-10-12 20:27:26 +0100

    updates4

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ divArray.forEach((cell) => {
   div.setAttribute(`id`, `${cell}`)
 })
 
-const playerCells = document.querySelectorAll('.mapsquare')
+let playerCells = document.querySelectorAll('.mapsquare')
 let pcCells = document.querySelectorAll('.mapSquarePc')
 
 
@@ -43,6 +43,7 @@ class boats {
   constructor(name, length) {
     this.name = name
     this.length = length
+    this.lives = length
   }
 
   hit() {
@@ -101,14 +102,12 @@ playerCells.forEach(square => square.addEventListener('drop', dragDrop))
 playerCells.forEach(square => square.addEventListener('dragend', () => {
 }))
 
-playerCells.forEach(cell => cell.addEventListener('click', (e) => {
-  console.log(e.target.id)
+playerCells.forEach(cell => cell.addEventListener('click', () => {
 }))
 
 playerShips.forEach(ship => ship.addEventListener('mousedown', (e) => {
   selectedShipCurrentId = e.target.id
   selectedShipCurrentId = Number(selectedShipCurrentId.replace(/\D/g, ''))
-  console.log(selectedShipCurrentId)
 }))
 
 
@@ -135,6 +134,7 @@ function dragDrop() {
 
   for (let i = 1; i < draggedShipLength + 1; i++) {
     playerCells[(targetSquare - width) + ((width * i)) - widthCorrection].setAttribute('id', `${shipClass}1`)
+    playerCells[(targetSquare - width) + ((width * i)) - widthCorrection].classList.add('taken')
   }
   boatYard.removeChild(draggedShip)
 
@@ -151,13 +151,12 @@ pcProfile.ships.push(new boats('Sub', 2))
 pcProfile.ships.push(new boats('Tug', 1))
 
 
-
+pcCells = Array.from(pcCells)
 
 function computerStart() {
   pcProfile.ships.forEach((ship) => {
     let placed = false
     while (placed === false) {
-      pcCells = Array.from(pcCells)
       const randomAxis = Math.floor(Math.random() * 2)
       let direction = 0
       if (randomAxis === 0) {
@@ -175,14 +174,12 @@ function computerStart() {
       const dissallowedCells = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99, 1, 2, 3, 4, 5, 6, 7, 8]
 
       const confirmCell = shipArray.some(r => dissallowedCells.indexOf(r) >= 0)
-      const isTaken = shipArray.some(r => isNaN(r) )
-      console.log(shipArray)
-      console.log(isTaken)
+      const isTaken = shipArray.some(r => isNaN(r))
       if (!isTaken && !confirmCell) {
         for (let i = 0; i < ship.length; i++) {
-          pcCells[(randomStart + (direction * i))].setAttribute('id', `${ship.name}1`)
+          pcCells[(randomStart + (direction * i))].setAttribute('id', `${ship.name}`)
           pcCells[(randomStart + (direction * i))].classList.add('taken')
-        } 
+        }
         placed = true
       } else {
         placed = false
@@ -192,3 +189,160 @@ function computerStart() {
 }
 
 computerStart()
+
+
+
+// ! Ship health and firing 
+
+
+let pcCarrierHealth = pcProfile.ships[0].lives
+let pcBattleshipHealth = pcProfile.ships[1].lives
+let pcCruiserHealth = pcProfile.ships[2].lives
+let pcSubHealth = pcProfile.ships[3].lives
+let pcTugHealth = pcProfile.ships[4].lives
+let pcLives = 4
+
+function sunk() {
+  console.log(pcLives)
+  if (pcLives === 0) {
+    console.log('game over')
+  } else {
+    return pcLives--
+  }
+}
+
+let cellship = null
+
+function hit(cellShip) {
+  if (cellShip === 'Carrier') {
+    if (pcCarrierHealth === 1) {
+      sunk()
+    } else {
+      pcCarrierHealth--
+    }
+
+  } else if (cellShip === 'Battleship') {
+    if (pcBattleshipHealth === 1) {
+      sunk()
+    } else pcBattleshipHealth--
+
+
+  } else if (cellShip === 'Cruiser') {
+    if (pcCruiserHealth === 1) {
+      sunk()
+    } else pcCruiserHealth--
+
+
+  } else if (cellShip === 'Sub') {
+    if (pcSubHealth === 1) {
+      sunk()
+    } else
+      pcSubHealth -= 1
+
+  } else if (cellShip === 'Tug') {
+    if (pcTugHealth === 1) {
+      sunk()
+    } else pcTugHealth--
+
+
+  }
+
+}
+
+
+const playerClick = pcCells.forEach(cell => cell.addEventListener('click', () => {
+  const cellShip = ((cell.id).substr(0, cell.id.length ))
+  if (cell.classList.contains('taken')) {
+    hit(cellShip)
+    cell.removeAttribute('id')
+    cell.classList.add('hit')
+  } else {
+    cell.classList.add('miss')
+  }
+
+
+  setTimeout(() => {
+    computerTurn()
+    playerClick
+  }, 500)
+
+})
+)
+
+
+// ! PC Firing 
+
+let playerCarrierHealth = userProfile.ships[0].lives
+let playerBattleshipHealth = userProfile.ships[1].lives
+let playerCruiserHealth = userProfile.ships[2].lives
+let playerSubHealth = userProfile.ships[3].lives
+let playerTugHealth = userProfile.ships[4].lives
+let playerLives = 4
+
+function playerSunk() {
+  console.log(playerLives)
+  if (playerLives === 0) {
+    console.log('game over')
+  } else {
+    return playerLives--
+  }
+}
+
+let PlayerCellShip = null
+
+function playerHit(PlayerCellShip) {
+  if (PlayerCellShip === 'Carrier') {
+    if (playerCarrierHealth === 1) {
+      playerSunk()
+    } else {
+      playerCarrierHealth--
+    }
+
+  } else if (PlayerCellShip === 'Battleship') {
+    if (playerBattleshipHealth === 1) {
+      playerSunk()
+    } else playerBattleshipHealth--
+
+
+  } else if (PlayerCellShip === 'Cruiser') {
+    if (playerCruiserHealth === 1) {
+      playerSunk()
+    } else playerCruiserHealth--
+
+
+  } else if (PlayerCellShip === 'Sub') {
+    if (playerSubHealth === 1) {
+      playerSunk()
+    } else
+      playerSubHealth -= 1
+
+  } else if (PlayerCellShip === 'Tug') {
+    if (playerTugHealth === 1) {
+      playerSunk()
+    } else playerTugHealth--
+  }
+
+}
+
+const computerTurnOne = null
+const computerTurnTwo = null
+
+function computerTurn() {
+
+  playerCells = Array.from(playerCells)
+
+  const availableCells = playerCells.filter(word => !word.classList.contains('miss', 'hit'))
+  const randomCell = Math.floor(Math.random() * availableCells.length)
+  const targetCell = availableCells[randomCell]
+  let PlayerCellShip = (targetCell.id).substr(0, targetCell.id.length - 1)
+  if (computerTurnOne === null && computerTurnTwo === null) {
+    if (availableCells[randomCell].classList.contains('taken')) {
+      playerHit(PlayerCellShip)
+      targetCell.classList.add('hit')
+      targetCell.removeAttribute(`id`)
+    } else {
+      targetCell.classList.add('miss')
+    }
+
+  }
+}
